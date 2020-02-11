22570cfc7962ec4fd307f2f18e7cdb9f1a3dd8cd
John Koo <johneverettkoo@gmail.com>
2020-02-11 10:29:58 -0500

    updated sdp functions

--- sdp-functions.R
+++ sdp-functions.R
@@ -1,53 +1,49 @@
-rc.sdp <- function(L, k = 2) {
+rc.sdp <- function(L, k = 2, verbose = FALSE, parallel = FALSE, cores = 4) {
+  if (parallel) {
+    doMC::registerDoMC(cores)
+  }
+  
   n <- nrow(L)
   
-  Z <- Variable(n, n, PSD = TRUE)
-  objective <- Minimize(matrix_trace(L %*% Z))
-  constraints <- list(matrix_trace(Z) == k,
+  Z <- CVXR::Variable(n, n, PSD = TRUE)
+  objective <- CVXR::Minimize(CVXR::matrix_trace(L %*% Z))
+  constraints <- list(CVXR::matrix_trace(Z) == k,
                       Z %*% rep(1, n) == rep(1, n))
-  positive.constraints <- plyr::alply(expand.grid(i = seq(n), j = seq(n)), 1,
-                                      function(row) {
-                                        if (row$i <= row$j) {
-                                          Z[row$i, row$j] >= 0
-                                        }
-                                      }) %>% 
-    plyr::compact()
-  constraints <- c(constraints, positive.constraints) %>% 
+  positive.constraints <- (Z >= 0)
+  constraints <- c(constraints, positive.constraints) %>%
     unname()
-  problem <- Problem(objective, constraints)
-  out <- solve(problem, parallel = FALSE)
+  problem <- CVXR::Problem(objective, constraints)
+  out <- CVXR::solve(problem, parallel = parallel, verbose = verbose)
   Z.hat <- out$getValue(Z)
   
   Z.eigen <- eigen(Z.hat, symmetric = TRUE)
   embedding <- Z.eigen$vectors %>% 
     sweep(2, Z.eigen$values, '*')
   clustering <- kmeans(embedding, 2)$cluster
-  return(clustering)
+  return(list(Z = Z.hat, clustering = clustering))
 }
 
-kmeans.sdp <- function(K, k = 2) {
+kmeans.sdp <- function(K, k = 2, verbose = FALSE, parallel = FALSE, cores = 4) {
+  if (parallel) {
+    doMC::registerDoMC(cores)
+  }
+  
   n <- nrow(K)
   
-  Z <- Variable(n, n, PSD = TRUE)
-  objective <- Maximize(matrix_trace(K %*% Z))
-  constraints <- list(matrix_trace(Z) == k,
+  Z <- CVXR::Variable(n, n, PSD = TRUE)
+  objective <- CVXR::Maximize(CVXR::matrix_trace(K %*% Z))
+  constraints <- list(CVXR::matrix_trace(Z) == k,
                       Z %*% rep(1, n) == rep(1, n))
-  positive.constraints <- plyr::alply(expand.grid(i = seq(n), j = seq(n)), 1,
-                                      function(row) {
-                                        if (row$i <= row$j) {
-                                          Z[row$i, row$j] >= 0
-                                        }
-                                      }) %>% 
-    plyr::compact()
+  positive.constraints <- (Z >= 0)
   constraints <- c(constraints, positive.constraints) %>% 
     unname()
-  problem <- Problem(objective, constraints)
-  out <- solve(problem, parallel = FALSE)
+  problem <- CVXR::Problem(objective, constraints)
+  out <- CVXR::solve(problem, parallel = parallel, verbose = verbose)
   Z.hat <- out$getValue(Z)
   
   Z.eigen <- eigen(Z.hat, symmetric = TRUE)
   embedding <- Z.eigen$vectors %>% 
     sweep(2, Z.eigen$values, '*')
   clustering <- kmeans(embedding, 2)$cluster
-  return(clustering)
+  return(list(Z = Z.hat, clustering = clustering))
 }
